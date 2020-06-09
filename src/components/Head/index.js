28d2de14f64384d2cf945e74ecae7036069d3a3d
Adrian Romero <radrian031613@gmail.com>
2020-06-09 15:16:39 -0700

    made changes to the grid ui and some styling

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -2,33 +2,19 @@ import React from 'react';
 import { Grid, Header, Icon } from 'semantic-ui-react';
 
 // import SearchBox from './../SearchBox';
-import './header.css';
+// import './header.css';
 
 const Head = () => (
-  <Grid.Column className='selfHeader'>
+
+  <Grid.Row className='selfHeader'>
     <Header as='h2' icon>
       <Icon name='settings' size='huge' />
     Employee Directory
-        </Header>
     <Header.Subheader>
       Search through the employees using.
       Use the input box to narrow your search.
           </Header.Subheader>
-  </Grid.Column>
-)
-// function Head() {
-//   return (
-//     <Grid.Column className='selfHeader'>
-//       <Header as='h2' icon>
-//         <Icon name='settings' size='huge' />
-//     Employee Directory
-//         </Header>
-//       <Header.Subheader>
-//         Search through the employees using.
-//         Use the input box to narrow your search.
-//           </Header.Subheader>
-//     </Grid.Column>
-//   )
-// };
+        </Header>
+  </Grid.Row>)
 
 export default Head;
